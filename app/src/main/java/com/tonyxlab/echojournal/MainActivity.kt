bcd4de0fa5569ae75373d02df2a4d315841fa956
Tonnie <vontonnie@gmail.com>
2025-01-02 08:36:22 +0300

    Create DummyViewModel

--- app/src/main/java/com/tonyxlab/echojournal/MainActivity.kt
+++ app/src/main/java/com/tonyxlab/echojournal/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,12 +12,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.tonyxlab.echojournal.ui.theme.EchoJournalTheme
+import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    val viewModel by viewModels<MainDummyViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        installSplashScreen().apply {
+
+            setKeepOnScreenCondition {!viewModel.isReady.value}
+        }
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             EchoJournalTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
