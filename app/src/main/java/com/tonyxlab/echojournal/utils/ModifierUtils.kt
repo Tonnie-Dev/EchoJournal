d9139e04ae518fb21065c21dae67f9ed768d0ba0
Tonnie <vontonnie@gmail.com>
2025-01-12 22:09:21 +0300

    Update ModifierUtils.kt

--- app/src/main/java/com/tonyxlab/echojournal/utils/ModifierUtils.kt
+++ app/src/main/java/com/tonyxlab/echojournal/utils/ModifierUtils.kt
@@ -1,15 +1,19 @@
 package com.tonyxlab.echojournal.utils
 
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 
-fun Modifier.addConditionalModifier(condition:Boolean, modifier:Modifier.()->Modifier):Modifier {
+@Composable
+fun Modifier.addConditionalModifier(
+    condition: Boolean,
+    modifier: @Composable Modifier.() -> Modifier
+): Modifier {
 
-    return if (condition){
+    return if (condition) {
 
         this.then(modifier())
-    }else this
-
+    } else this
 
 
 }
