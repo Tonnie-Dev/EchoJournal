ccde85a3ce479d53fdc2045cdf1fab693b7b2c9e
Tonnie <vontonnie@gmail.com>
2025-01-20 21:05:56 +0300

    Create AppIcon.kt

---
+++ app/src/main/java/com/tonyxlab/echojournal/presentation/components/AppIcon.kt
@@ -0,0 +1,26 @@
+package com.tonyxlab.echojournal.presentation.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+
+@Composable
+fun AppIcon(
+    modifier: Modifier = Modifier,
+    imageVector: ImageVector = Icons.Default.Add,
+    tint: Color = MaterialTheme.colorScheme.onPrimary,
+    contentDescription: String = ""
+) {
+    Icon(
+        modifier = modifier,
+        imageVector = imageVector,
+        contentDescription = contentDescription,
+        tint = tint
+    )
+
+}
