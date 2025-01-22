84a7477cf02526148d5652fed56183251b1262a1
Tonnie <vontonnie@gmail.com>
2025-01-22 21:06:56 +0300

    Update TopicSelector.kt

---
+++ app/src/main/java/com/tonyxlab/echojournal/presentation/components/BasicEntryTextField.kt
@@ -0,0 +1,155 @@
+package com.tonyxlab.echojournal.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.tonyxlab.echojournal.R
+import com.tonyxlab.echojournal.presentation.ui.theme.EchoJournalTheme
+import com.tonyxlab.echojournal.presentation.ui.theme.Secondary70
+import com.tonyxlab.echojournal.presentation.ui.theme.Secondary95
+import com.tonyxlab.echojournal.presentation.ui.theme.spacing
+
+@Composable
+private fun BasicEntryTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    hint: String,
+    modifier: Modifier = Modifier,
+    iconContent: @Composable (() -> Unit)? = null,
+    hintColor: Color = MaterialTheme.colorScheme.outlineVariant,
+    isHeadline: Boolean = false,
+    gap: Dp = 6.dp,
+) {
+    val focusManager = LocalFocusManager.current
+    val bodyMediumTextStyle = TextStyle(
+        color = MaterialTheme.colorScheme.outlineVariant,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.W400
+    )
+    var isFocused by remember { mutableStateOf(false) }
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(gap),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        iconContent?.invoke()
+
+        BasicTextField(
+            modifier = Modifier.onFocusChanged { isFocused = it.isFocused },
+            value = value,
+            onValueChange = onValueChange,
+            textStyle = bodyMediumTextStyle,
+            decorationBox = {
+                Box(contentAlignment = Alignment.Center) {
+
+                    if (value.isBlank() && isFocused.not()) {
+                        Text(
+                            modifier = Modifier.fillMaxWidth(),
+                            text = hint,
+                            color = hintColor,
+                            style = if (isHeadline)
+                                MaterialTheme.typography.headlineLarge
+                            else
+                                MaterialTheme.typography.bodyMedium
+                        )
+                    } else if (value.isNotBlank()) {
+                        Box(
+                            modifier = Modifier.fillMaxWidth(),
+                            contentAlignment = Alignment.CenterStart
+                        ) {
+                            it()
+
+                        }
+                    }
+                }
+            }
+        )
+
+    }
+
+}
+
+
+@PreviewLightDark
+@Composable
+private fun EntryBasicTextFieldPreview() {
+
+    EchoJournalTheme {
+
+        Column(
+            modifier = Modifier
+                .background(color = MaterialTheme.colorScheme.surfaceVariant)
+                .padding(MaterialTheme.spacing.spaceMedium),
+            verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.spaceMedium)
+        ) {
+
+            BasicEntryTextField(
+                value = "",
+                onValueChange = {},
+                isHeadline = true,
+                hint = "Add Something ...",
+                iconContent = {
+                    AppIcon(
+                        modifier = Modifier
+                            .clip(CircleShape)
+                            .background(color = Secondary95)
+                            .size(MaterialTheme.spacing.spaceLarge),
+                        imageVector = Icons.Default.Add,
+                        tint = Secondary70
+                    )
+                }
+            )
+
+            BasicEntryTextField(
+                value = "Work",
+                onValueChange = {},
+                isHeadline = true,
+                hint = "Add Something ...",
+                iconContent = {
+                    AppIcon(
+                        modifier = Modifier
+                            .size(
+                                width = MaterialTheme.spacing.spaceMedium,
+                                height = MaterialTheme.spacing.spaceExtraSmall * 5
+                            ),
+                        imageVector = Icons.Default.Edit,
+                        tint = MaterialTheme.colorScheme.outlineVariant
+                    )
+                }
+            )
+        }
+    }
+}
--- app/src/main/java/com/tonyxlab/echojournal/presentation/save/TopicSelector.kt
+++ app/src/main/java/com/tonyxlab/echojournal/presentation/save/TopicSelector.kt
@@ -6,20 +6,34 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.SoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.tonyxlab.echojournal.R
+import com.tonyxlab.echojournal.presentation.components.AppIcon
 import com.tonyxlab.echojournal.presentation.ui.theme.NeutralVariant10
 import com.tonyxlab.echojournal.presentation.ui.theme.NeutralVariant30
 import com.tonyxlab.echojournal.presentation.ui.theme.Primary95
@@ -35,8 +49,7 @@ private fun TopicsListing(modifier: Modifier = Modifier) {
             .shadow(
                 elevation = MaterialTheme.spacing.spaceSmall,
                 shape = MaterialTheme.shapes.medium,
-            ),
-        color = Secondary90
+            ), color = Secondary90
     ) {
 
         Column(
@@ -50,8 +63,7 @@ private fun TopicsListing(modifier: Modifier = Modifier) {
                 color = NeutralVariant10,
             )
 
-            IconButton(
-                onClick = {},
+            IconButton(onClick = {},
                 modifier = modifier
                     .padding(vertical = MaterialTheme.spacing.spaceSmall)
                     .background(color = Primary95, shape = CircleShape)
@@ -62,9 +74,100 @@ private fun TopicsListing(modifier: Modifier = Modifier) {
                         contentDescription = null,
                         tint = NeutralVariant30,
                     )
-                }
-            )
+                })
         }
     }
 
 }
+
+@Composable
+fun ChipFlowRow(
+    isAddingTopic: Boolean,
+    onAddTopic: (Boolean) -> Unit,
+    searchQuery: String,
+    onSearch: (String) -> Unit,
+    focusRequester: FocusRequester,
+    modifier: Modifier = Modifier,
+    keyboardController: SoftwareKeyboardController? = null,
+) {
+
+
+    LaunchedEffect(key1 = isAddingTopic) {
+        // Do not show keyboard when not adding a topic 
+        if (isAddingTopic.not()) return@LaunchedEffect
+        focusRequester.requestFocus()
+        keyboardController?.show()
+
+    }
+
+    // Show Icon when not typing
+
+    if (!isAddingTopic) {
+
+        IconButton(onClick = {},
+            modifier = modifier
+                .padding(vertical = MaterialTheme.spacing.spaceSmall)
+                .background(color = Primary95, shape = CircleShape)
+                .size(32.dp),
+            content = {
+                Icon(
+                    Icons.Default.Add,
+                    contentDescription = null,
+                    tint = NeutralVariant30,
+                )
+            })
+
+    }
+    // Show TextField if typing 
+    else {
+
+        BasicTextField(
+            modifier = Modifier
+                .wrapContentWidth()
+                .padding(top = MaterialTheme.spacing.spaceMedium)
+                .focusRequester(focusRequester),
+            value = searchQuery,
+            onValueChange = onSearch,
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text, imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(onDone = {
+                onAddTopic(false)
+                keyboardController?.hide()
+            })
+        )
+    }
+}
+
+
+@Composable
+fun TopicChip(
+    topic: String,
+    modifier: Modifier = Modifier,
+    onCancel: (() -> Unit)? = null
+) {
+
+
+    FilterChip(
+        modifier = modifier,
+        selected = true,
+        onClick = {},
+        shape = MaterialTheme.shapes.large,
+        colors = FilterChipDefaults.filterChipColors(
+            selectedContainerColor = MaterialTheme.colorScheme.onPrimaryContainer
+        ),
+        leadingIcon = {
+            AppIcon(
+                modifier = Modifier.size(MaterialTheme.spacing.spaceSmall),
+                imageVector = Icons.Outlined.Info,
+                tint = MaterialTheme.colorScheme.inverseOnSurface
+            )
+        }, label = {
+
+            Text(text = topic, style = m)
+        }
+    )
+}
+
+
