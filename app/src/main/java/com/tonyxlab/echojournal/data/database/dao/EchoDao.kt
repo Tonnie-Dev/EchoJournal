1fe97c6ec2a0dbf8fab2d64b1a3a7ca876505f86
Tonnie <vontonnie@gmail.com>
2025-01-28 11:19:00 +0300

    Add EchoDao interface

---
+++ app/src/main/java/com/tonyxlab/echojournal/data/database/dao/EchoDao.kt
@@ -0,0 +1,18 @@
+package com.tonyxlab.echojournal.data.database.dao
+
+import androidx.room.Query
+import com.tonyxlab.echojournal.data.database.entity.EchoEntity
+import kotlinx.coroutines.flow.Flow
+
+interface EchoDao:BaseDao<EchoEntity> {
+
+    @Query("SELECT * FROM echo_table ORDER BY timestamp DESC")
+    fun getEchos():Flow<List<EchoEntity>>
+
+
+    @Query("SELECT * FROM echo_table WHERE id=:id")
+    fun getEchoById(id:String): EchoEntity?
+
+
+
+}
--- app/src/main/java/com/tonyxlab/echojournal/data/database/entity/EchoEntity.kt
+++ app/src/main/java/com/tonyxlab/echojournal/data/database/entity/EchoEntity.kt
@@ -1,9 +1,11 @@
 package com.tonyxlab.echojournal.data.database.entity
 
 import androidx.room.ColumnInfo
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.tonyxlab.echojournal.domain.model.Mood
 
+@Entity(tableName = "echo_table")
 data class EchoEntity(
     @PrimaryKey(autoGenerate = false)
     @ColumnInfo(name = "id")
@@ -11,15 +13,15 @@ data class EchoEntity(
     @ColumnInfo(name = "name")
     val name: String,
     @ColumnInfo(name = "description")
-    val description:String,
+    val description: String,
     @ColumnInfo(name = "timestamp")
-    val timestamp:Long,
+    val timestamp: Long,
     @ColumnInfo(name = "length")
     val length: Int,
     @ColumnInfo(name = "mood")
     val mood: Mood,
     @ColumnInfo(name = "topics")
-    val topics:List<String>,
+    val topics: List<String>,
     @ColumnInfo(name = "uri")
     val uri: String
 )
