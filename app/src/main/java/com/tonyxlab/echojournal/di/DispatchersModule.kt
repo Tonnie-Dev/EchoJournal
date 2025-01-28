fd949c5d1bde6dbbe1cbe864d159f08ddedaeb39
Tonnie <vontonnie@gmail.com>
2025-01-28 12:57:32 +0300

    Add Dispatchers Module

---
+++ app/src/main/java/com/tonyxlab/echojournal/di/DispatchersModule.kt
@@ -0,0 +1,20 @@
+package com.tonyxlab.echojournal.di
+
+import com.tonyxlab.echojournal.utils.AppCoroutineDispatchers
+import dagger.Module
+import dagger.Provides
+import kotlinx.coroutines.Dispatchers
+
+
+@Module
+object DispatchersModule {
+    @Provides
+    fun provideCoroutineDispatchers(): AppCoroutineDispatchers {
+
+        return AppCoroutineDispatchers(
+                main = Dispatchers.Main,
+                io = Dispatchers.IO,
+                computation = Dispatchers.Unconfined
+        )
+    }
+}
