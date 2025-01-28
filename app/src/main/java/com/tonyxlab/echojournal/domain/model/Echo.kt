caf087d49169f455f153baa395317545f5fa3b4a
Tonnie <vontonnie@gmail.com>
2025-01-28 08:26:39 +0300

    Update Echo.kt

--- app/src/main/java/com/tonyxlab/echojournal/domain/model/Echo.kt
+++ app/src/main/java/com/tonyxlab/echojournal/domain/model/Echo.kt
@@ -1,4 +1,14 @@
 package com.tonyxlab.echojournal.domain.model
 
+import java.sql.Timestamp
 
-data class Echo(val id:String)
+
+data class Echo(
+    val id: String,
+    val name: String,
+    val description: String,
+    val timeStamp: Long,
+    val length: Int,
+    val mood: Mood,
+    val topics: List<String>
+)
