518c7d5aadb486a46826af615aa7958e132a471a
Tonnie <vontonnie@gmail.com>
2025-01-28 16:10:25 +0300

    Add CreateEchoUseCase class

---
+++ app/src/main/java/com/tonyxlab/echojournal/domain/usecases/CreateEchoUseCase.kt
@@ -0,0 +1,14 @@
+package com.tonyxlab.echojournal.domain.usecases
+
+import com.tonyxlab.echojournal.domain.model.Echo
+import com.tonyxlab.echojournal.domain.repository.EchoRepository
+import com.tonyxlab.echojournal.utils.Resource
+import javax.inject.Inject
+
+class CreateEchoUseCase @Inject constructor(private val repository: EchoRepository) {
+
+    suspend operator fun invoke(echo: Echo): Resource<Boolean> {
+
+        return repository.createEcho(echo)
+    }
+}
