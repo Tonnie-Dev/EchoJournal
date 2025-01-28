4f96c7292fc5a150ae2a32b772d8f5465d547e6d
Tonnie <vontonnie@gmail.com>
2025-01-28 13:33:49 +0300

    Add EchoRepository interface definition

--- app/src/main/java/com/tonyxlab/echojournal/domain/repository/EchoRepository.kt
+++ app/src/main/java/com/tonyxlab/echojournal/domain/repository/EchoRepository.kt
@@ -1,10 +1,15 @@
 package com.tonyxlab.echojournal.domain.repository
 
 import com.tonyxlab.echojournal.domain.model.Echo
+import com.tonyxlab.echojournal.utils.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface EchoRepository {
 
-    fun getEchos():Flow<List<Echo>>
-    fun getEchoById(id:String):Echo
+    fun getEchos(): Flow<List<Echo>>
+    fun getEchoById(id: String): Resource<Echo>
+    fun createEcho(echo: Echo): Resource<Boolean>
+    fun updateEcho(echo: Echo): Resource<Boolean>
+    fun deleteEcho(echo: Echo): Resource<Boolean>
+    
 }
