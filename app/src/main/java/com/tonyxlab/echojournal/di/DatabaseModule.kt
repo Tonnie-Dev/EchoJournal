8bf88ed8f4cc18e712cf4f4764536070429f5080
Tonnie <vontonnie@gmail.com>
2025-01-28 12:34:02 +0300

    Update Database Module

--- app/src/main/java/com/tonyxlab/echojournal/di/DatabaseModule.kt
+++ app/src/main/java/com/tonyxlab/echojournal/di/DatabaseModule.kt
@@ -3,6 +3,9 @@ package com.tonyxlab.echojournal.di
 import android.content.Context
 import androidx.room.Room
 import com.tonyxlab.echojournal.data.database.EchoDatabase
+import com.tonyxlab.echojournal.data.database.converters.Converters
+import com.tonyxlab.echojournal.data.database.dao.EchoDao
+import com.tonyxlab.echojournal.domain.json.JsonSerializer
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,15 +15,30 @@ import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-object DatabaseModule{
+object DatabaseModule {
 
     private val DATABASE_NAME = "echo_database"
 
 
     @Provides
-    fun provideEchoDatabase(@ApplicationContext context: Context):EchoDatabase{
+    fun provideEchoDatabase(
+        @ApplicationContext context: Context,
+        jsonSerializer: JsonSerializer
+    ): EchoDatabase {
 
+        return Room.databaseBuilder(
+                context = context,
+                klass = EchoDatabase::class.java,
+                name = DATABASE_NAME
+        )
+                .addTypeConverter(Converters(serializer = jsonSerializer))
+                .fallbackToDestructiveMigration(false)
+                .build()
+    }
 
-        return Room.databaseBuilder(context,EchoDatabase::class.java, DATABASE_NAME).build()
+    @Provides
+    fun provideDao(database: EchoDatabase): EchoDao {
+
+        return database.getDao()
     }
 }
