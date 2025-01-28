b5c443c86db322006afef182c63ac063c96e575d
Tonnie <vontonnie@gmail.com>
2025-01-28 16:07:32 +0300

    Add GetEchosUseCase class

---
+++ app/src/main/java/com/tonyxlab/echojournal/domain/usecases/GetEchosUseCase.kt
@@ -0,0 +1,14 @@
+package com.tonyxlab.echojournal.domain.usecases
+
+import com.tonyxlab.echojournal.domain.model.Echo
+import com.tonyxlab.echojournal.domain.repository.EchoRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetEchosUseCase @Inject constructor(private val repository: EchoRepository) {
+
+    operator fun invoke(): Flow<List<Echo>> {
+
+        return repository.getEchos()
+    }
+}
