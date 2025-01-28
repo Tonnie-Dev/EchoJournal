f0f48cb1f9bdd7da5bf65c24088793f84b042b72
Tonnie <vontonnie@gmail.com>
2025-01-28 09:45:13 +0300

    Add JsonSerializerImpl class

---
+++ app/src/main/java/com/tonyxlab/echojournal/data/jsonimpl/JsonSerializerImpl.kt
@@ -0,0 +1,20 @@
+package com.tonyxlab.echojournal.data.jsonimpl
+
+import com.tonyxlab.echojournal.domain.json.JsonSerializer
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import javax.inject.Inject
+
+class JsonSerializerImpl : JsonSerializer {
+    override fun <T> toJson(serializer: KSerializer<T>, data: T): String {
+        return Json.encodeToString(serializer, data)
+    }
+
+    override fun <T> fromJson(serializer: KSerializer<T>, json: String): T {
+        return Json.decodeFromString(serializer, json)
+
+    }
+
+
+}
