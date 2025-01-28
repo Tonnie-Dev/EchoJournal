621d1154f27945f8172fca1990dd934679a850fc
Tonnie <vontonnie@gmail.com>
2025-01-28 17:03:15 +0300

    Create DateUtils.kt

---
+++ app/src/main/java/com/tonyxlab/echojournal/utils/DateUtils.kt
@@ -0,0 +1,42 @@
+package com.tonyxlab.echojournal.utils
+
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toInstant
+import kotlinx.datetime.toLocalDateTime
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+
+fun LocalDateTime.Companion.now(): LocalDateTime {
+    val instant = Clock.System.now()
+    return instant.toLocalDateTime(TimeZone.currentSystemDefault())
+}
+
+fun LocalDateTime.fromLocalDateTimeToUtcTimeStamp(): Long {
+
+    return this.toInstant(timeZone = TimeZone.currentSystemDefault())
+        .toLocalDateTime(timeZone = TimeZone.currentSystemDefault())
+        .toInstant(TimeZone.UTC)
+        .toEpochMilliseconds()
+
+}
+
+fun Long.fromUtcTimeStampToLocalDateTime(): LocalDateTime {
+
+    return Instant.fromEpochMilliseconds(this)
+        .toLocalDateTime(TimeZone.UTC)
+        .toInstant(TimeZone.currentSystemDefault())
+        .toLocalDateTime(TimeZone.currentSystemDefault())
+}
+
+fun Long.toAmPmTime(): String {
+
+    val pattern = "hh:mm aa"
+    val date = Date(this)
+
+    return SimpleDateFormat(pattern, Locale.getDefault()).format(date)
+}
