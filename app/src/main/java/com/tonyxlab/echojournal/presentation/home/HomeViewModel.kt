d90f484db0943d24ec64f295097eb66aa0c107b8
Tonnie <vontonnie@gmail.com>
2025-01-29 09:36:41 +0300

    Create HomeViewModel.kt

---
+++ app/src/main/java/com/tonyxlab/echojournal/presentation/home/HomeViewModel.kt
@@ -0,0 +1,53 @@
+package com.tonyxlab.echojournal.presentation.home
+
+import android.net.Uri
+import androidx.lifecycle.ViewModel
+import com.tonyxlab.echojournal.domain.model.Echo
+import com.tonyxlab.echojournal.utils.TextFieldValue
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor() : ViewModel() {
+
+    private val _echoes = MutableStateFlow<List<Echo>>(emptyList())
+    val echoes = _echoes.asStateFlow()
+
+    private val _isPlaying = MutableStateFlow(false)
+    val isPlaying = _isPlaying.asStateFlow()
+
+
+    private val _uiState = MutableStateFlow(UiState())
+    val uiState = _uiState.asStateFlow()
+
+
+    var seekFieldValue = MutableStateFlow(
+        TextFieldValue(
+            value = _uiState.value.seekValue,
+            onValueChange = this::onSeek
+
+        )
+    )
+        private set
+
+
+    fun onCreateEcho() {}
+
+    fun onSeek(value: Float) {}
+
+    fun play(uri: Uri) {
+
+
+        _isPlaying.value = true
+    }
+
+
+    fun stop() {
+
+
+        _isPlaying.value = false
+    }
+
+}
