be31f1924a59882ffee4ae8110b0b3422eac9227
Tonnie <vontonnie@gmail.com>
2025-02-01 14:01:14 +0300

    Update UiState.kt

--- app/src/main/java/com/tonyxlab/echojournal/presentation/home/UiState.kt
+++ app/src/main/java/com/tonyxlab/echojournal/presentation/home/UiState.kt
@@ -2,6 +2,7 @@ package com.tonyxlab.echojournal.presentation.home
 
 import android.net.Uri
 import com.tonyxlab.echojournal.domain.model.Echo
+import com.tonyxlab.echojournal.domain.model.Mood
 
 data class UiState(
     val echoes: List<Echo> = emptyList(),
@@ -15,5 +16,11 @@ data class UiState(
     val isRecordingActivated: Boolean = false,
     val isRecordingInProgress: Boolean = false,
     val recordingUri: Uri = Uri.EMPTY,
-    val isShowMoodSelectionSheet:Boolean = false
-)
+    val isShowMoodSelectionSheet: Boolean = false,
+    val isShowMoodTitleIcon: Boolean = false,
+    val mood: Mood = Mood.Other
+) {
+
+    var isMoodConfirmButtonHighlighted = mood != Mood.Other
+
+}
