6d78743f99525df52c6ff565d2bb196c88c26ed8
Tonnie <vontonnie@gmail.com>
2025-02-01 09:22:25 +0300

    Update AppButton.kt

--- app/src/main/java/com/tonyxlab/echojournal/presentation/components/AppButton.kt
+++ app/src/main/java/com/tonyxlab/echojournal/presentation/components/AppButton.kt
@@ -1,13 +1,13 @@
 package com.tonyxlab.echojournal.presentation.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -19,6 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.contentcapture.ContentCaptureManager.Companion.isEnabled
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -28,11 +29,12 @@ import com.tonyxlab.echojournal.presentation.ui.theme.EchoJournalTheme
 import com.tonyxlab.echojournal.presentation.ui.theme.buttonMediumTextStyle
 import com.tonyxlab.echojournal.presentation.ui.theme.gradient
 import com.tonyxlab.echojournal.presentation.ui.theme.spacing
-import com.tonyxlab.echojournal.utils.addConditionalModifier
+import com.tonyxlab.echojournal.utils.conditionalModifier
 
 @Composable
 fun AppButton(
     buttonText: String,
+     onClick:()-> Unit,
     modifier: Modifier = Modifier,
     isEnabled: Boolean = true,
     isHighlighted: Boolean = false,
@@ -44,16 +46,16 @@ fun AppButton(
         modifier = modifier
             .clip(RoundedCornerShape(MaterialTheme.spacing.spaceMedium))
 
-            .addConditionalModifier(isEnabled) {
+            .conditionalModifier(isEnabled) {
                 background(MaterialTheme.colorScheme.onPrimaryContainer)
             }
-            .addConditionalModifier(isEnabled.not()) {
+            .conditionalModifier(isEnabled.not()) {
                 background(MaterialTheme.colorScheme.surfaceVariant)
             }
-            .addConditionalModifier(isHighlighted) {
+            .conditionalModifier(isHighlighted) {
 
                 background(brush = MaterialTheme.gradient.buttonPressedGradient)
-            },
+            }.clickable { onClick() },
 
         contentAlignment = Alignment.Center
 
@@ -109,10 +111,11 @@ private fun AppButtonPreview() {
             verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.spaceMedium)
         ) {
 
-            AppButton(buttonText = stringResource(id = R.string.text_cancel))
-            AppButton(buttonText = stringResource(id = R.string.text_save), isEnabled = false)
-            AppButton(buttonText = stringResource(id = R.string.text_confirm), isHighlighted = true)
+            AppButton(onClick = {}, buttonText = stringResource(id = R.string.text_cancel))
+            AppButton(onClick = {}, buttonText = stringResource(id = R.string.text_save), isEnabled = false)
+            AppButton(onClick = {}, buttonText = stringResource(id = R.string.text_confirm), isHighlighted = true)
             AppButton(
+                onClick = {},
                 buttonText = stringResource(id = R.string.text_confirm),
                 isEnabled = true,
                 isHighlighted = true,
