a0b231c253273a97789bc498196c68cffe9cf704
Tonnie <vontonnie@gmail.com>
2025-02-02 11:30:37 +0300

    Update HomeViewModel.kt

--- app/src/main/java/com/tonyxlab/echojournal/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/tonyxlab/echojournal/presentation/home/HomeViewModel.kt
@@ -2,20 +2,12 @@ package com.tonyxlab.echojournal.presentation.home
 
 import android.content.Context
 import android.net.Uri
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.navigation.toRoute
 import com.tonyxlab.echojournal.domain.audio.AudioPlayer
 import com.tonyxlab.echojournal.domain.audio.AudioRecorder
 import com.tonyxlab.echojournal.domain.model.Echo
-import com.tonyxlab.echojournal.domain.model.Mood
-import com.tonyxlab.echojournal.domain.usecases.CreateEchoUseCase
-import com.tonyxlab.echojournal.domain.usecases.GetEchoByIdUseCase
 import com.tonyxlab.echojournal.domain.usecases.GetEchoesUseCase
-import com.tonyxlab.echojournal.domain.usecases.UpdateEchoUseCase
-import com.tonyxlab.echojournal.presentation.navigation.SaveScreenObject
-import com.tonyxlab.echojournal.utils.Resource
 import com.tonyxlab.echojournal.utils.TextFieldValue
 import com.tonyxlab.echojournal.utils.fromLocalDateTimeToUtcTimeStamp
 import com.tonyxlab.echojournal.utils.now
@@ -26,11 +18,8 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import kotlinx.datetime.LocalDateTime
-import timber.log.Timber
 import java.io.File
-import java.util.UUID
 import javax.inject.Inject
 
 @HiltViewModel
@@ -38,195 +27,62 @@ class HomeViewModel @Inject constructor(
     @ApplicationContext private val context: Context,
     private val recorder: AudioRecorder,
     private val player: AudioPlayer,
-    private val getEchoByIdUseCase: GetEchoByIdUseCase,
     getEchoesUseCase: GetEchoesUseCase,
-    private val updateEchoUseCase: UpdateEchoUseCase,
-    private val createEchoUseCase: CreateEchoUseCase,
-    savedStateHandle: SavedStateHandle
-) : ViewModel() {
 
-    var echo: Echo? = null
+
+) : ViewModel() {
 
     private val _echoes = MutableStateFlow<List<Echo>>(emptyList())
     val echoes = _echoes.asStateFlow()
 
     init {
 
-        val echoId = savedStateHandle.toRoute<SaveScreenObject>().id
-
-        Timber.i("Id is $echoId")
-        readEchoInfo(echoId = echoId)
         getEchoesUseCase().onEach {
 
             _echoes.value = it
 
         }.launchIn(viewModelScope)
 
-
-
     }
-    private val _uiState = MutableStateFlow(UiState())
-    val uiState = _uiState.asStateFlow()
+
+
+    private val _homeState = MutableStateFlow(HomeState())
+    val homeUiState = _homeState.asStateFlow()
 
     var seekFieldValue = MutableStateFlow(
         TextFieldValue(
-            value = _uiState.value.seekValue,
-            onValueChange = this::onSeek
+            value = _homeState.value.seekValue,
+            onValueChange = this::setSeek
 
         )
     )
         private set
-
-
-    var titleFieldValue = MutableStateFlow(
-        TextFieldValue(
-            value = _uiState.value.title,
-            onValueChange = this::setTitle
-
-        )
-    )
-        private set
-
-    var topicFieldValue = MutableStateFlow(
-        TextFieldValue(
-            value = _uiState.value.topic,
-            onValueChange = this::setTopic
-
-        )
-    )
-        private set
-    var descriptionFieldValue = MutableStateFlow(
-        TextFieldValue(
-            value = _uiState.value.description,
-            onValueChange = this::setDescription
-
-        )
-    )
-        private set
-
-
-    private fun readEchoInfo(echoId: String?) {
-
-        echoId ?: return
-
-        viewModelScope.launch {
-
-            when (val result = getEchoByIdUseCase(echoId)) {
-
-
-                is Resource.Success -> {
-
-                    echo = result.data
-
-                    with(result.data) {
-
-                        _uiState.update {
-                            it.copy(
-                                currentTopics = topics,
-                                description = description,
-                                title = title,
-
-
-                                )
-                        }
-
-                        setTitle(this.title)
-                        setMood(mood)
-                       setDescription(description)
-                    }
-                }
-
-                is Resource.Error -> Unit
-            }
-        }
-
-
-    }
-
-    fun doSave() {
-
-        val echoItem = Echo(
-            id = this.echo?.id ?: UUID.randomUUID().toString(),
-            title = titleFieldValue.value.value,
-            description = descriptionFieldValue.value.value,
-            timestamp = LocalDateTime.now().fromLocalDateTimeToUtcTimeStamp(),
-            length = 0,
-            mood = _uiState.value.mood,
-            topics = listOf(),
-            uri = _uiState.value.recordingUri
-        )
-
-
-        viewModelScope.launch {
-
-
-            val result = if (this@HomeViewModel.echo != null) {
-
-                updateEchoUseCase(echo = echoItem)
-            } else {
-                createEchoUseCase(echo = echoItem)
-            }
-
-            when (result) {
-
-                is Resource.Success -> {
-
-                    Timber.i("Saved")
-                }
-
-                is Resource.Error -> {
-
-                    Timber.i("Saving Failed: ${result.exception.message}")
-                }
-            }
-        }
-
-
-    }
 
     fun createEcho() {
 
-        _uiState.update { it.copy(isRecordingActivated = true) }
+        _homeState.update { it.copy(isRecordingActivated = true) }
     }
 
-    fun dismissRecordingModalSheet() {
-
-
-        _uiState.update { it.copy(isRecordingActivated = false) }
-    }
-
-    fun showMoodSelectionSheet() {
-
-        _uiState.update { it.copy(isShowMoodSelectionSheet = true) }
-
-    }
-
-    fun dismissMoodSelectionModalSheet() {
-
-
-        _uiState.update { it.copy(isShowMoodSelectionSheet = false) }
-    }
-
-    fun onSeek(value: Float) {}
 
     fun play(uri: Uri) {
 
-       
 
-        _uiState.update { it.copy(isPlaying = true) }
+        _homeState.update { it.copy(isPlaying = true) }
     }
 
 
     fun stop() {
 
-        _uiState.update { it.copy(isPlaying = false) }
+        _homeState.update { it.copy(isPlaying = false) }
 
     }
 
+    fun dismissRecordingModalSheet() {
+
+        _homeState.update { it.copy(isRecordingActivated = false) }
+    }
 
     fun startRecording() {
-
-        Timber.i("Start recording Called")
 
         val file = File(
             context.filesDir,
@@ -238,55 +94,20 @@ class HomeViewModel @Inject constructor(
             }
 
         val fileUri = Uri.fromFile(file)
-        _uiState.update { it.copy(recordingUri = fileUri) }
+      //  _homeState.update { it.copy(recordingUri = fileUri) }
     }
 
     fun stopRecording() {
 
-        Timber.i("Stop recording Called")
         recorder.stop()
     }
 
-    fun pauseRecording() {}
-
-    private fun setTitle(value: String) {
-
-
-        titleFieldValue.update { it.copy(value = value) }
-
-    }
-
-    private fun setTopic(value: String) {
-
-        topicFieldValue.update { it.copy(value = value) }
-
-    }
-
-    private fun setDescription(value: String) {
-
-        descriptionFieldValue.update { it.copy(value = value) }
-
-    }
-
-
-    fun setMood(value: Mood) {
-
-        _uiState.update { it.copy(mood = value) }
-
-    }
-
-    fun confirmMoodSelection() {
-
-        setMood(uiState.value.mood)
-
-        _uiState.update { it.copy(isShowMoodTitleIcon = true) }
-    }
-
-    fun canSave(): Boolean {
-
-        return _uiState.value.mood != Mood.Other && titleFieldValue.value.value.isNotBlank()
+    fun pauseRecording()  {
 
 
     }
+
+    fun setSeek(value: Float) {}
+
 }
 
