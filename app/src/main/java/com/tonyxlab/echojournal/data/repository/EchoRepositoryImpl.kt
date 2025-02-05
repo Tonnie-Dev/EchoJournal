7903dfaa2e472296e933e48b9b0dc1fb3f50af7f
Tonnie <vontonnie@gmail.com>
2025-02-05 19:59:48 +0300

    Update EchoRepositoryImpl.kt

--- app/src/main/java/com/tonyxlab/echojournal/data/repository/EchoRepositoryImpl.kt
+++ app/src/main/java/com/tonyxlab/echojournal/data/repository/EchoRepositoryImpl.kt
@@ -1,7 +1,10 @@
 package com.tonyxlab.echojournal.data.repository
 
+
+
 import com.tonyxlab.echojournal.data.database.dao.EchoDao
 import com.tonyxlab.echojournal.data.mappers.toDomainModel
+import com.tonyxlab.echojournal.data.mappers.toEchoWithTopics
 import com.tonyxlab.echojournal.data.mappers.toEntityModel
 import com.tonyxlab.echojournal.domain.model.Echo
 import com.tonyxlab.echojournal.domain.repository.EchoRepository
@@ -16,50 +19,47 @@ class EchoRepositoryImpl @Inject constructor(
     private val dao: EchoDao,
     private val dispatchers: AppCoroutineDispatchers
 ) : EchoRepository {
-    override fun getEchos(): Flow<List<Echo>> {
 
-        return dao.getEchos().map {
-            it.map { entity -> entity.toDomainModel() }
+    override fun getEchos(): Flow<List<Echo>> {
+        return dao.getEchoesWithTopics().map { echoWithTopicsList ->
+            echoWithTopicsList.map { echoWithTopics ->
+                echoWithTopics.toDomainModel()
+            }
+        }
+    }
+
+    override suspend fun insertEchoWithTopics(echo: Echo): Resource<Boolean> {
+        return withContext(dispatchers.io) {
+            return@withContext try {
+
+                val existingTopics = dao.getTopicsByEchoId(echo.id)
+                val echoWithTopics = echo.toEchoWithTopics(existingTopics                                                                                )
+                dao.insertEchoWithTopics(echoWithTopics)
+                Resource.Success(true)
+            } catch (e: Exception) {
+                Resource.Error(e)
+            }
         }
     }
 
     override suspend fun getEchoById(id: String): Resource<Echo> {
-
         return withContext(dispatchers.io) {
-
             return@withContext try {
-                dao.getEchoById(id = id)?.let {
-
+                dao.getEchoWithTopicsById(id = id)?.let {
                     Resource.Success(it.toDomainModel())
                 } ?: Resource.Error(KotlinNullPointerException())
             } catch (e: Exception) {
                 Resource.Error(exception = e)
             }
         }
-
-    }
-
-    override suspend fun createEcho(echo: Echo): Resource<Boolean> {
-
-        return withContext(dispatchers.io) {
-
-            return@withContext try {
-                val result = dao.insert(echo.toEntityModel())
-                Resource.Success(result != -1L)
-            } catch (e: Exception) {
-
-                Resource.Error(e)
-            }
-        }
-
     }
 
     override suspend fun updateEcho(echo: Echo): Resource<Boolean> {
-
         return withContext(dispatchers.io) {
-
             return@withContext try {
-                dao.update(echo.toEntityModel())
+
+                val existingTopics = dao.getTopicsByEchoId(echo.id)
+                dao.updateEchoWithTopics(echo.toEchoWithTopics(existingTopics))
                 Resource.Success(true)
             } catch (e: Exception) {
                 Resource.Error(e)
@@ -73,10 +73,8 @@ class EchoRepositoryImpl @Inject constructor(
                 val result = dao.delete(echo.toEntityModel())
                 Resource.Success(result != -1)
             } catch (e: Exception) {
-
                 Resource.Error(e)
             }
-
         }
     }
 }
