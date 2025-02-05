d082ae6528307090b9dc70822ea08087a2c8ac20
Tonnie <vontonnie@gmail.com>
2025-02-05 19:59:41 +0300

    Update EchoMappers.kt

--- app/src/main/java/com/tonyxlab/echojournal/data/mappers/EchoMappers.kt
+++ app/src/main/java/com/tonyxlab/echojournal/data/mappers/EchoMappers.kt
@@ -2,6 +2,8 @@ package com.tonyxlab.echojournal.data.mappers
 
 import android.net.Uri
 import com.tonyxlab.echojournal.data.database.entity.EchoEntity
+import com.tonyxlab.echojournal.data.database.entity.EchoWithTopics
+import com.tonyxlab.echojournal.data.database.entity.TopicEntity
 import com.tonyxlab.echojournal.domain.model.Echo
 import com.tonyxlab.echojournal.utils.fromUtcTimestampToDefaultTimeStamp
 import com.tonyxlab.echojournal.utils.toUtcTimeStamp
@@ -15,12 +17,12 @@ fun Echo.toEntityModel(): EchoEntity {
         timestamp = timestamp.toUtcTimeStamp(),
         length = length,
         mood = mood,
-        topics = topics,
+
         uri = uri.toString()
     )
 }
 
-fun EchoEntity.toDomainModel(): Echo {
+fun EchoEntity.toDomainModel(topics:List<TopicEntity>): Echo {
     return Echo(
         id = id,
         title = title,
@@ -28,7 +30,23 @@ fun EchoEntity.toDomainModel(): Echo {
         timestamp = timestamp.fromUtcTimestampToDefaultTimeStamp(),
         length = length,
         mood = mood,
-        topics = topics,
+        topics = topics.map { it.topic },
         uri = Uri.parse(uri)
     )
+}
+
+
+fun Echo.toEchoWithTopics(existingTopics: List<TopicEntity>): EchoWithTopics {
+    val topicEntities = this.topics.map { topic ->
+        val existingTopic = existingTopics.find { it.topic == topic }
+        existingTopic?.copy(echoId = id) ?: TopicEntity(echoId = id, topic = topic)
+    }
+    return EchoWithTopics(echoEntity = this.toEntityModel(), topicEntities = topicEntities)
+}
+
+
+
+fun EchoWithTopics.toDomainModel(): Echo {
+
+ return echoEntity.toDomainModel(topicEntities)
 }
