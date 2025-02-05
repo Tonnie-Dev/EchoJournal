bfe999ed39f050555b3fe60a21b90ae31149f6e5
Tonnie <vontonnie@gmail.com>
2025-02-05 19:59:51 +0300

    Update EchoRepository.kt

--- app/src/main/java/com/tonyxlab/echojournal/domain/repository/EchoRepository.kt
+++ app/src/main/java/com/tonyxlab/echojournal/domain/repository/EchoRepository.kt
@@ -1,15 +1,17 @@
 package com.tonyxlab.echojournal.domain.repository
 
+import com.tonyxlab.echojournal.data.database.entity.EchoWithTopics
 import com.tonyxlab.echojournal.domain.model.Echo
 import com.tonyxlab.echojournal.utils.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface EchoRepository {
 
-    fun getEchos(): Flow<List<Echo>>
+    fun getEchos():Flow<List<Echo>>
+    //fun getSavedTopics():Flow<List<String>>
     suspend fun getEchoById(id: String): Resource<Echo>
-    suspend fun createEcho(echo: Echo): Resource<Boolean>
+    //suspend fun createEcho(echo: Echo): Resource<Boolean>
     suspend fun updateEcho(echo: Echo): Resource<Boolean>
     suspend fun deleteEcho(echo: Echo): Resource<Boolean>
-
+    suspend fun insertEchoWithTopics(echo: Echo):Resource<Boolean>
 }
