3e519411b7fc39fb9648bbc4640c085c11ca3ace
Tonnie <vontonnie@gmail.com>
2025-02-05 19:59:09 +0300

    Update BaseDao.kt

--- app/src/main/java/com/tonyxlab/echojournal/data/database/dao/BaseDao.kt
+++ app/src/main/java/com/tonyxlab/echojournal/data/database/dao/BaseDao.kt
@@ -1,19 +1,23 @@
 package com.tonyxlab.echojournal.data.database.dao
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Update
 
 
+@Dao
 interface BaseDao<T> {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(obj: T): Long
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(value: T): Long
-
-    @Delete
-    fun delete(value: T): Int
+    suspend fun insert(obj: List<T>)
 
     @Update
-    fun update(value: T)
+    suspend fun update(obj: T)
+
+    @Delete
+    suspend fun delete(obj: T): Int
 }
