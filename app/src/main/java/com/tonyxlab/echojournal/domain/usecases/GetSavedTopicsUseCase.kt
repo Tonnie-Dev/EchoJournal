50fad2b5abd5491d8daadf44139e90ec1ca131b4
Tonnie <vontonnie@gmail.com>
2025-02-05 19:59:58 +0300

    Create GetSavedTopicsUseCase.kt

---
+++ app/src/main/java/com/tonyxlab/echojournal/domain/usecases/GetSavedTopicsUseCase.kt
@@ -0,0 +1,12 @@
+package com.tonyxlab.echojournal.domain.usecases
+
+import com.tonyxlab.echojournal.domain.repository.EchoRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+/*
+class GetSavedTopicsUseCase @Inject constructor(private val repository: EchoRepository) {
+    operator fun invoke(): Flow<List<String>> {
+        return repository.getSavedTopics()
+    }
+}*/
