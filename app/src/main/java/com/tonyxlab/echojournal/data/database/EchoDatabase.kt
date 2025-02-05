42fb1451d0af2e072c5f5f3b4447894ef28e025a
Tonnie <vontonnie@gmail.com>
2025-02-05 19:59:32 +0300

    Update EchoDatabase.kt

--- app/src/main/java/com/tonyxlab/echojournal/data/database/EchoDatabase.kt
+++ app/src/main/java/com/tonyxlab/echojournal/data/database/EchoDatabase.kt
@@ -6,8 +6,9 @@ import androidx.room.TypeConverters
 import com.tonyxlab.echojournal.data.database.converters.Converters
 import com.tonyxlab.echojournal.data.database.dao.EchoDao
 import com.tonyxlab.echojournal.data.database.entity.EchoEntity
+import com.tonyxlab.echojournal.data.database.entity.TopicEntity
 
-@Database(entities = [EchoEntity::class], version = 1, exportSchema = false)
+@Database(entities = [EchoEntity::class, TopicEntity::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class EchoDatabase:RoomDatabase(){
 
