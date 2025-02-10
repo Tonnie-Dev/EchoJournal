c7faca982e80d49d3eaa92a3496954870a8082b7
Tonnie <vontonnie@gmail.com>
2025-02-10 20:34:42 +0300

    Add StopWatch util class

---
+++ app/src/main/java/com/tonyxlab/echojournal/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.tonyxlab.echojournal.utils
+
+object Constants {
+
+
+    const val DEFAULT_FORMATTED_TIME = "00:00:00"
+}
---
+++ app/src/main/java/com/tonyxlab/echojournal/utils/StopWatch.kt
@@ -0,0 +1,55 @@
+package com.tonyxlab.echojournal.utils
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class StopWatch @Inject constructor(private val appCoroutineDispatchers: AppCoroutineDispatchers) {
+
+    private val _formattedTime = MutableStateFlow(Constants.DEFAULT_FORMATTED_TIME)
+    val formattedTime: StateFlow<String> = _formattedTime
+
+    private var coroutineScope = CoroutineScope(appCoroutineDispatchers.main)
+    private var isRunning = false
+
+    private var timeMillis = 0L
+    private var lastTimestamp = 0L
+
+    fun start() {
+
+        if (isRunning) return
+
+        coroutineScope.launch {
+            lastTimestamp = System.currentTimeMillis()
+            isRunning = true
+            while (isRunning) {
+                delay(10L)
+                timeMillis += System.currentTimeMillis() - lastTimestamp
+                lastTimestamp = System.currentTimeMillis()
+                _formattedTime.value = timeMillis.formatMillisToTime()
+            }
+        }
+    }
+
+
+    fun pause() {
+
+        isRunning = false
+    }
+
+
+    fun stop() {
+
+        coroutineScope.cancel()
+        coroutineScope = CoroutineScope(appCoroutineDispatchers.main)
+        timeMillis = 0L
+        lastTimestamp = 0L
+        _formattedTime.value = Constants.DEFAULT_FORMATTED_TIME
+        isRunning = false
+    }
+
+}
