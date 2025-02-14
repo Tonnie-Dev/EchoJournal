48cb6a0ea9a9a027cbaf487f3c46feb436507ed8
Tonnie <vontonnie@gmail.com>
2025-02-14 21:32:09 +0300

    Create HomeUiState.kt

---
+++ app/src/main/java/com/tonyxlab/echojournal/presentation/screens/home/handling/HomeUiState.kt
@@ -0,0 +1,47 @@
+package com.tonyxlab.echojournal.presentation.screens.home.handling
+
+import androidx.compose.runtime.Stable
+import com.tonyxlab.echojournal.domain.model.Echo
+import com.tonyxlab.echojournal.domain.model.Mood
+import com.tonyxlab.echojournal.presentation.core.base.handling.UiState
+import com.tonyxlab.echojournal.presentation.core.state.PlayerState
+import com.tonyxlab.echojournal.utils.Constants
+
+
+@Stable
+data class HomeUiState(
+    val entries: Map<Long, List<EchoHolderState>> = mapOf(),
+    val filter: FilterState = FilterState(),
+    val isFilterActive: Boolean = false,
+    val homeSheetState: HomeSheetState = HomeSheetState(),
+    val isPermissionDialogOpen: Boolean = false
+) : UiState {
+
+
+    @Stable
+    data class EchoHolderState(
+        val echo: Echo,
+        val playerState: PlayerState = PlayerState(duration = echo.length)
+    )
+
+    @Stable
+    data class HomeSheetState(
+        val isVisibleBoolean: Boolean = false,
+        val isRecording: Boolean = true,
+        val recordingTime: String = Constants.DEFAULT_FORMATTED_TIME
+    )
+
+    @Stable
+    data class FilterState(
+        val isMoodFilterSelected: Boolean = false,
+        val isTopicFilterSelected: Boolean = false,
+        val moodFilterItems: List<FilterItem> = Mood.allMoods().map { FilterItem(title = it.name) },
+        val topicFilterItems: List<FilterItem> = listOf()
+    ) {
+
+        data class FilterItem(
+            val title: String = "",
+            val isChecked: Boolean = false
+        )
+    }
+}
