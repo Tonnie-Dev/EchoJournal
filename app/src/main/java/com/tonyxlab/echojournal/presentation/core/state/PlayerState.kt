7757ac6b562fae38bb192f617802321dec08daf2
Tonnie <vontonnie@gmail.com>
2025-02-21 06:42:53 +0300

    Update PlayerState.kt

--- app/src/main/java/com/tonyxlab/echojournal/presentation/core/state/PlayerState.kt
+++ app/src/main/java/com/tonyxlab/echojournal/presentation/core/state/PlayerState.kt
@@ -9,17 +9,17 @@ data class PlayerState(
     val duration: Int = 0,
     val currentPosition: Int = 0,
     val currentPositionText: String = "00:00",
-    val action: Action = Action.Initializing
+    val action: Action = Action.Stopped
 ) {
 
     val durationText = duration.toLong().formatMillisToTime()
 
     sealed interface Action {
 
-        data object Initializing : Action
         data object Playing : Action
         data object Paused : Action
         data object Resumed : Action
+        data object Stopped : Action
     }
 
 }
