836d3d1d1f69c1082a2c65340627135e0c55ad84
Tonnie <vontonnie@gmail.com>
2025-02-25 19:15:04 +0300

    Add TopicChip comosable

--- app/src/main/java/com/tonyxlab/echojournal/presentation/screens/home/components/EchoHolder.kt
+++ app/src/main/java/com/tonyxlab/echojournal/presentation/screens/home/components/EchoHolder.kt
@@ -1,13 +1,27 @@
 package com.tonyxlab.echojournal.presentation.screens.home.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
+import com.tonyxlab.echojournal.presentation.core.utils.spacing
+import com.tonyxlab.echojournal.presentation.theme.EchoJournalTheme
+import com.tonyxlab.echojournal.presentation.theme.EchoUltraLightGray
 
 @Composable
 private fun EntryHeader(
@@ -31,5 +45,58 @@ private fun EntryHeader(
             text = creationTime,
             style = MaterialTheme.typography.labelMedium
         )
+    }
+}
+
+
+@Composable
+private fun TopicChip(
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .height(MaterialTheme.spacing.spaceDoubleDp * 11)
+            .clip(CircleShape)
+            .background(
+                color = EchoUltraLightGray,
+                shape = CircleShape
+            ), contentAlignment = Alignment.CenterStart
+    ) {
+
+        Text(
+            modifier = Modifier.padding(MaterialTheme.spacing.spaceSmall),
+            text = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = .5f)
+                    )
+                ) {
+                    append("#")
+                }
+                append(title)
+            },
+            style = MaterialTheme.typography.bodySmall
+        )
+    }
+}
+
+@Composable
+private fun TopicChipPreview() {
+
+
+    EchoJournalTheme {
+
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            TopicChip(
+                title = "Tonnie",
+
+                )
+        }
     }
 }
