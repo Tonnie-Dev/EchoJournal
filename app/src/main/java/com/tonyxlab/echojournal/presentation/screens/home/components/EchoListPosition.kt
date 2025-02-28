750b31033d6f1238927449605583007b69ad8fcb
Tonnie <vontonnie@gmail.com>
2025-02-28 08:47:43 +0300

    Update EchoListPosition.kt

--- app/src/main/java/com/tonyxlab/echojournal/presentation/screens/home/components/EchoListPosition.kt
+++ app/src/main/java/com/tonyxlab/echojournal/presentation/screens/home/components/EchoListPosition.kt
@@ -1,8 +1,8 @@
 package com.tonyxlab.echojournal.presentation.screens.home.components
 
 sealed interface EchoListPosition {
-    data object Top:EchoListPosition
-    data object Center:EchoListPosition
-    data object Bottom:EchoListPosition
+    data object First:EchoListPosition
+    data object Middle:EchoListPosition
+    data object Last:EchoListPosition
     data object Single:EchoListPosition
 }
