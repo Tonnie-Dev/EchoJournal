2156fc584f457348a0ebb616b1db81273111060a
Tonnie <vontonnie@gmail.com>
2025-02-28 19:06:28 +0300

    Add FiterList composable

--- app/src/main/java/com/tonyxlab/echojournal/presentation/screens/home/components/FilterList.kt
+++ app/src/main/java/com/tonyxlab/echojournal/presentation/screens/home/components/FilterList.kt
@@ -2,11 +2,17 @@ package com.tonyxlab.echojournal.presentation.screens.home.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
@@ -15,26 +21,73 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.IntOffset
 import com.tonyxlab.echojournal.R
 import com.tonyxlab.echojournal.domain.model.Mood
 import com.tonyxlab.echojournal.domain.model.toMood
 import com.tonyxlab.echojournal.presentation.core.utils.spacing
-import com.tonyxlab.echojournal.presentation.screens.home.handling.HomeUiState
+import com.tonyxlab.echojournal.presentation.screens.home.handling.HomeUiState.FilterState
 
 @Composable
-fun FilterList(modifier: Modifier = Modifier) {
+fun FilterList(
+    filterItems: List<FilterState.FilterItem>,
+    onItemClick: (String) -> Unit,
+    onDismissClicked: () -> Unit,
+    modifier: Modifier = Modifier,
+    startOffset: IntOffset = IntOffset.Zero
+) {
 
+
+    Box(modifier = modifier
+        .offset {
+            startOffset
+        }
+        .fillMaxSize()) {
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null
+                ) { onDismissClicked() }
+        )
+
+        Surface(
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(MaterialTheme.spacing.spaceTen),
+            shadowElevation = MaterialTheme.spacing.spaceExtraSmall
+        ) {
+            LazyColumn(
+                modifier = Modifier.padding(MaterialTheme.spacing.spaceExtraSmall),
+                verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.spaceExtraSmall)
+            ) {
+
+                items(filterItems) { filterItem ->
+
+                    FilterItem(
+                        filterItem = filterItem,
+                        onClick = { onItemClick(it) })
+
+                }
+
+            }
+
+        }
+
+    }
 }
 
 
 @Composable
 fun FilterItem(
-    filterItem: HomeUiState.FilterState.FilterItem,
+    filterItem: FilterState.FilterItem,
     onClick: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
