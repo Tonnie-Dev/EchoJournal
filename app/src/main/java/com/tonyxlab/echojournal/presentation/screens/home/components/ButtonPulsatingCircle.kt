1b76950667ebd2e8c8395c0b36fbd06596c1eb69
Tonnie <vontonnie@gmail.com>
2025-03-02 18:13:32 +0300

    Create ButtonPulsatingCircle.kt

---
+++ app/src/main/java/com/tonyxlab/echojournal/presentation/screens/home/components/ButtonPulsatingCircle.kt
@@ -0,0 +1,67 @@
+package com.tonyxlab.echojournal.presentation.screens.home.components
+
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.InfiniteRepeatableSpec
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+import com.tonyxlab.echojournal.presentation.core.utils.Gradients
+import com.tonyxlab.echojournal.presentation.core.utils.spacing
+
+@Composable
+
+fun ButtonPulsatingCircle(
+    modifier: Modifier = Modifier,
+    baseSize: Dp = MaterialTheme.spacing.spaceOneHundred + MaterialTheme.spacing.spaceSmall,
+    pulseSize: Dp = MaterialTheme.spacing.spaceOneTwentyEight,
+) {
+
+
+    val infiniteTransition = rememberInfiniteTransition(label = "Pulsating Circle Transition")
+    val density = LocalDensity.current
+
+    val baseSizePx = with(density) { baseSize.toPx() }
+    val pulsatingSizePx = with(density) { pulseSize.toPx() }
+
+
+    val animatedSize by infiniteTransition.animateFloat(
+        initialValue = baseSizePx,
+        targetValue = pulsatingSizePx,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 1_0000, easing = FastOutSlowInEasing),
+            repeatMode = RepeatMode.Reverse
+        ),
+        label = "Pulsating Circle Size"
+    )
+
+
+    Canvas(modifier = modifier.size(pulseSize)) {
+
+       // Circle with Animation
+
+        drawCircle(
+            brush = Gradients.GradientPressed,
+            radius = animatedSize/2,
+            center = Offset(x = size.width/2, y = size.height/2)
+        )
+
+       // Circle without animation
+        drawCircle(
+            brush = Gradients.GradientPressed,
+            radius =baseSizePx/2,
+            center = Offset(x = size.width/2, y = size.height)
+        )
+    }
+
+}
