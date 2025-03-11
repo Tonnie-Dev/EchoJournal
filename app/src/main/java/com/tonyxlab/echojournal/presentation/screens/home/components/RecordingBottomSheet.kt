acfae3a7adeb10f2fbff19b8cb27b00b9258b5ba
Tonnie <vontonnie@gmail.com>
2025-03-11 20:26:24 +0300

    Add RecordButtons

--- app/src/main/java/com/tonyxlab/echojournal/presentation/screens/home/components/RecordingBottomSheet.kt
+++ app/src/main/java/com/tonyxlab/echojournal/presentation/screens/home/components/RecordingBottomSheet.kt
@@ -1,13 +1,26 @@
 package com.tonyxlab.echojournal.presentation.screens.home.components
 
-import android.R.attr.text
+import android.R.attr.action
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
@@ -15,11 +28,14 @@ import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
 import com.tonyxlab.echojournal.R
+import com.tonyxlab.echojournal.presentation.core.utils.gradient
 import com.tonyxlab.echojournal.presentation.core.utils.spacing
-import com.tonyxlab.echojournal.presentation.screens.home.HomeScreenRoot
 import com.tonyxlab.echojournal.presentation.screens.home.handling.HomeUiEvent
 import com.tonyxlab.echojournal.presentation.screens.home.handling.HomeUiState
 
@@ -94,4 +110,91 @@ private fun BottomSheetHeader(
             )
         }
     }
+}
+
+
+@Composable
+fun RecordButtons(
+    isRecording: Boolean,
+    onCancelClick: () -> Unit,
+    onRecordClick: () -> Unit,
+    onPauseClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    recordButtonSize: Dp = MaterialTheme.spacing.spaceTwelve * 6,
+    auxiliaryButtonSize: Dp = MaterialTheme.spacing.spaceTwelve * 4
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(recordButtonSize),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+
+        // Cancel Button
+        IconButton(
+            modifier = Modifier.size(auxiliaryButtonSize),
+            onClick = onCancelClick,
+            colors = IconButtonDefaults.iconButtonColors(
+                containerColor = MaterialTheme.colorScheme.errorContainer,
+                contentColor = MaterialTheme.colorScheme.onErrorContainer
+            )
+        ) {
+            Icon(
+                imageVector = Icons.Default.Clear,
+                contentDescription = stringResource(id = R.string.cancel_recording)
+            )
+        }
+
+        // Record Button
+        Box(
+            modifier = Modifier
+                .size(recordButtonSize)
+                .clip(CircleShape)
+                .background(
+                    brush = MaterialTheme.gradient.buttonPressedGradient,
+                    shape = CircleShape
+                )
+                .clickable { onRecordClick() },
+            contentAlignment = Alignment.Center
+
+        ) {
+
+            Image(
+                painter = if (isRecording)
+                    painterResource(R.drawable.ic_checkmark)
+                else
+                    painterResource(R.drawable.ic_recording),
+                contentDescription = stringResource(id = R.string.recording_button)
+            )
+
+            if (isRecording) {
+
+                ButtonPulsatingCircle()
+            }
+        }
+
+        // Pause Button
+        IconButton(
+
+            modifier = Modifier.size(auxiliaryButtonSize),
+            onClick = onPauseClick,
+            colors = IconButtonDefaults.iconButtonColors(
+                containerColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                contentColor = MaterialTheme.colorScheme.primary
+            )
+
+        ) {
+            Icon(
+                painter = if (isRecording)
+                    painterResource(R.drawable.ic_pause)
+                else
+                    painterResource(R.drawable.ic_checkmark_primary),
+                contentDescription = stringResource(id = R.string.pause_text)
+            )
+        }
+
+
+    }
+
 }
