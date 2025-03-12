f9978ac570935f6e8b62b040ccc99e2124a6431f
Tonnie <vontonnie@gmail.com>
2025-03-12 20:16:34 +0300

    Add Topic Data Class

---
+++ app/src/main/java/com/tonyxlab/echojournal/domain/model/Topic.kt
@@ -0,0 +1,6 @@
+package com.tonyxlab.echojournal.domain.model
+
+data class Topic(
+    val id: Int = Constants.INITIAL_TOPIC_ID,
+    val name: String
+)
--- app/src/main/java/com/tonyxlab/echojournal/utils/Constants.kt
+++ app/src/main/java/com/tonyxlab/echojournal/utils/Constants.kt
@@ -2,6 +2,6 @@ package com.tonyxlab.echojournal.utils
 
 object Constants {
 
-
     const val DEFAULT_FORMATTED_TIME = "00:00:00"
+    const val INITIAL_TOPIC_ID = 0L
 }
