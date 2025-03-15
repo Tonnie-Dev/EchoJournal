07824c57b8c9f89630276cc171887b1ad50dd742
Tonnie <vontonnie@gmail.com>
2025-03-15 16:48:47 +0300

    Update RepositoryModule.kt

--- app/src/main/java/com/tonyxlab/echojournal/di/RepositoryModule.kt
+++ app/src/main/java/com/tonyxlab/echojournal/di/RepositoryModule.kt
@@ -1,17 +1,28 @@
 package com.tonyxlab.echojournal.di
 
 import com.tonyxlab.echojournal.data.repository.EchoRepositoryImpl
+import com.tonyxlab.echojournal.data.repository.TopicRepositoryImpl
 import com.tonyxlab.echojournal.domain.repository.EchoRepository
+import com.tonyxlab.echojournal.domain.repository.TopicRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
 
     @Binds
-    abstract fun bindEchoRepository(echoRepositoryImpl: EchoRepositoryImpl):EchoRepository
+    abstract fun bindTopicRepository(
+        topicRepositoryImpl: TopicRepositoryImpl
+    ): TopicRepository
+
+    @Binds
+    abstract fun bindEchoRepository(
+        echoRepositoryImpl: EchoRepositoryImpl
+    ): EchoRepository
+
 
 }
