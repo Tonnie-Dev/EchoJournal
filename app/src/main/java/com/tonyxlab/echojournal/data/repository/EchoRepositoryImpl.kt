941aebd572f788f86b8401884e6da13ac7c2828f
Tonnie <vontonnie@gmail.com>
2025-03-15 16:17:55 +0300

    Update EchoREpositoryImpl

--- app/src/main/java/com/tonyxlab/echojournal/data/repository/EchoRepositoryImpl.kt
+++ app/src/main/java/com/tonyxlab/echojournal/data/repository/EchoRepositoryImpl.kt
@@ -1,3 +1,24 @@
 package com.tonyxlab.echojournal.data.repository
 
+import com.tonyxlab.echojournal.data.local.dao.EchoDao
+import com.tonyxlab.echojournal.data.local.mappers.toEchoEntity
+import com.tonyxlab.echojournal.data.local.mappers.toEchoesList
+import com.tonyxlab.echojournal.domain.model.Echo
+import com.tonyxlab.echojournal.domain.repository.EchoRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
 
+class EchoRepositoryImpl @Inject constructor(private val dao: EchoDao) : EchoRepository {
+    override fun getEchos(): Flow<List<Echo>> {
+        return dao.getEchoes().map { it.toEchoesList() }
+    }
+
+    override suspend fun upsertEcho(echo: Echo) {
+        dao.upsert(echo.toEchoEntity())
+    }
+
+    override suspend fun deleteEcho(echo: Echo) {
+        dao.delete(echo.toEchoEntity())
+    }
+}
