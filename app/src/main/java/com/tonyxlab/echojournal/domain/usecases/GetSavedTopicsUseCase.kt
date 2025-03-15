c725917320b16ad477ab448bad6d558c9ae62072
Tonnie <vontonnie@gmail.com>
2025-03-15 15:48:23 +0300

    Delete Usecases classess

--- app/src/main/java/com/tonyxlab/echojournal/domain/usecases/GetSavedTopicsUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.tonyxlab.echojournal.domain.usecases
-
-/*
-class GetSavedTopicsUseCase @Inject constructor(private val repository: EchoRepository) {
-    operator fun invoke(): Flow<List<String>> {
-        return repository.getSavedTopics()
-    }
-}*/
