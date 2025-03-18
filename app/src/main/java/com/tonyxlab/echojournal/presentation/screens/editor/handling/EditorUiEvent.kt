215f360769d7c81b0cf857f2310f17a669bf373a
Tonnie <vontonnie@gmail.com>
2025-03-18 19:55:22 +0300

    Add EntryUiEvent sealed interface

--- app/src/main/java/com/tonyxlab/echojournal/presentation/screens/editor/handling/EditorUiEvent.kt
+++ app/src/main/java/com/tonyxlab/echojournal/presentation/screens/editor/handling/EditorUiEvent.kt
@@ -1,13 +1,34 @@
 package com.tonyxlab.echojournal.presentation.screens.editor.handling
 
 import com.tonyxlab.echojournal.domain.model.Mood
+import com.tonyxlab.echojournal.domain.model.Topic
 import com.tonyxlab.echojournal.presentation.core.base.handling.UiEvent
+import java.io.File
 
-sealed interface EditorUiEvent:UiEvent {
+sealed interface EditorUiEvent : UiEvent {
 
-    data object BottomSheetOpened: EditorUiEvent
-    data object BottomSheetClosed: EditorUiEvent
-   data class MoodSelected(val mood:Mood): EditorUiEvent
+    data class BottomSheetOpened(val mood: Mood) : EditorUiEvent
+    data object BottomSheetClosed : EditorUiEvent
+    data class SheetConfirmClicked(val mood: Mood) : EditorUiEvent
 
+    data class MoodSelected(val mood: Mood) : EditorUiEvent
+    data class TitleValueChanged(val titleValue: String) : EditorUiEvent
+    data class DescriptionValueChanged(val descValue: String) : EditorUiEvent
+
+
+    data class TopicValueChanged(val topicValue: String) : EditorUiEvent
+    data class TagClearClicked(val topic: Topic) : EditorUiEvent
+
+    data class TopicSelected(val topic: Topic) : EditorUiEvent
+    data object CreateTopicClicked : EditorUiEvent
+
+    data object PlayClicked : EditorUiEvent
+    data object PauseClicked : EditorUiEvent
+    data object ResumeClicked : EditorUiEvent
+
+    data class SaveButtonClicked(val outDir: File) : EditorUiEvent
+
+    data object ExitDialogToggled : EditorUiEvent
+    data object ExitDialogConfirmClicked : EditorUiEvent
 
 }
