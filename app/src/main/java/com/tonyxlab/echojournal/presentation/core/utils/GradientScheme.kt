2b13eb4d513ff8bc1abb46d637a2233d8f66b777
Tonnie <vontonnie@gmail.com>
2025-03-23 18:20:37 +0300

    Update GradientScheme.kt

--- app/src/main/java/com/tonyxlab/echojournal/presentation/core/utils/GradientScheme.kt
+++ app/src/main/java/com/tonyxlab/echojournal/presentation/core/utils/GradientScheme.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import com.tonyxlab.echojournal.presentation.theme.SurfaceVariant
 
 data class GradientScheme(
     val buttonDefaultGradient: Brush = DefaultGradient,
@@ -15,14 +16,17 @@ data class GradientScheme(
     companion object {
 
         val DefaultGradient = Brush.linearGradient(
-                colors = listOf(Color(0xFF578CFF), Color(0xFF1F70F5))
+            colors = listOf(Color(0xFF578CFF), Color(0xFF1F70F5))
         )
 
         val PressedGradient = Brush.linearGradient(
-                colors = listOf(Color(0xFF578CFF), Color(0xFF0057CC))
+            colors = listOf(Color(0xFF578CFF), Color(0xFF0057CC))
         )
 
-
+        val DisabledSolidColo = Brush.linearGradient(
+            colors = listOf(SurfaceVariant, SurfaceVariant)
+        )
+        
         val FabRecordingBackground = Brush.verticalGradient(
             colors = listOf(
                 Color(0xFF3982F6).copy(alpha = 0.2f),
