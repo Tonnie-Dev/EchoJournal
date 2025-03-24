0375738c24d44e379cb9bc86aa9b21c0dbbc9278
Tonnie <vontonnie@gmail.com>
2025-03-24 19:18:49 +0300

    Update TopicsDatabase.kt

--- app/src/main/java/com/tonyxlab/echojournal/data/local/database/TopicsDatabase.kt
+++ app/src/main/java/com/tonyxlab/echojournal/data/local/database/TopicsDatabase.kt
@@ -4,9 +4,10 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.tonyxlab.echojournal.data.local.dao.TopicsDao
 import com.tonyxlab.echojournal.data.local.entity.EchoEntity
+import com.tonyxlab.echojournal.data.local.entity.TopicEntity
 
 
-@Database(entities = [EchoEntity::class], version = 1, exportSchema = false)
+@Database(entities = [TopicEntity::class], version = 1, exportSchema = false)
 abstract class TopicsDatabase: RoomDatabase(){
 
     abstract fun getTopicsDao(): TopicsDao
