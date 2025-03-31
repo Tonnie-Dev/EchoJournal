7bc1a2396e318d08b6f82770fbd9adee1687d10a
Tonnie <vontonnie@gmail.com>
2025-03-31 11:57:20 +0300

    Create SettingActionEvent.kt

---
+++ app/src/main/java/com/tonyxlab/echojournal/presentation/screens/settings/handling/SettingActionEvent.kt
@@ -0,0 +1,5 @@
+package com.tonyxlab.echojournal.presentation.screens.settings.handling
+
+import com.tonyxlab.echojournal.presentation.core.base.handling.ActionEvent
+
+sealed interface SettingActionEvent : ActionEvent
