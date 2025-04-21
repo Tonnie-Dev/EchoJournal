d03778821f8f161a09c3ec977d6960cd2d60af0f
Tonnie <vontonnie@gmail.com>
2025-04-21 16:42:42 +0300

    Bump up version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.tonyxlab.echojournal"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.1.0"
+        versionCode = 5
+        versionName = "1.225752.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
